84869cc1d4b145b1efb6d4ce97bfcc6fd3ccf24a
Timur Umurzakov <goust327@gmail.com>
2023-05-26 19:45:44 +0600

    Добавил регистрацию

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,13 +4,14 @@ import LoginModal from "@component/modals/login";
 import IUser from "@component/models/IUser";
 import userApi from "@component/mixin/userApi";
 import Image from "next/image";
-import Link from "next/link";
 import Cookies from "js-cookie";
+import Register from "@component/modals/register";
 
 const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
     const [showModal, setShowModal] = useState(false);
+    const [showRegisterModal, setShowRegisterModal] = useState(false);
     const [user, setUser] = useState<IUser | null>(null);
 
     useEffect(() => {
@@ -44,6 +45,14 @@ const NavBar = () => {
         setShowModal(false);
     };
 
+    const handleOpenRegModal = () => {
+        setShowRegisterModal(true);
+    };
+
+    const handleCloseRegModal = () => {
+        setShowRegisterModal(false);
+    };
+
     const handleLogout = async (e: FormEvent) => {
         e.preventDefault();
 
@@ -56,23 +65,32 @@ const NavBar = () => {
         }
     };
     return (
-        <Navbar  className={scrolled ? "scrolled" : ""}>
+        <Navbar className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="/">
-                    <Image width={240} height={100} src='/assets/img/logo.svg' alt="Logo" />
+                    <Image width={240} height={100} src='/assets/img/logo.svg' alt="Logo"/>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} >Главная</Nav.Link>
-                        {!user && <Nav.Link onClick={handleOpenModal} className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Войти</Nav.Link>}
+                        <Nav.Link href="/"
+                                  className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Главная</Nav.Link>
+                        {
+                            !user && (
+                                <div className="d-flex flex-row">
+                                    <Nav.Link onClick={handleOpenModal}
+                                              className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Войти</Nav.Link>
+                                    <Nav.Link onClick={handleOpenRegModal}
+                                              className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}>Регистрация</Nav.Link>
+                                </div>
+                            )
+                        }
                         {user && (
                             <NavDropdown className={'nav-user'} title={user.firstName} id="user-dropdown">
-                                <NavDropdown.Item>
-                                    <Link href={'/profile'}>Профиль</Link>
-                                </NavDropdown.Item>
+                                <NavDropdown.Item href="/profile">Профиль</NavDropdown.Item>
+                                <NavDropdown.Item href="/profile/bookings">Брони</NavDropdown.Item>
                                 <NavDropdown.Item href="/profile/cart">Корзина</NavDropdown.Item>
                                 <NavDropdown.Item href="/profile/support">Поддержка</NavDropdown.Item>
                                 <NavDropdown.Item onClick={handleLogout}>Выйти</NavDropdown.Item>
@@ -81,15 +99,16 @@ const NavBar = () => {
                         )}
                     </Nav>
                     <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src='/assets/img/nav-icon1.svg' alt="" /></a>
-                <a href="#"><img src='/assets/img/nav-icon2.svg' alt="" /></a>
-                <a href="#"><img src='/assets/img/nav-icon3.svg' alt="" /></a>
-              </div>
-            </span>
+                      <div className="social-icon">
+                        <a href="#"><Image width={16} height={16} src='/assets/img/nav-icon1.svg' alt=""/></a>
+                        <a href="#"><Image width={16} height={16} src='/assets/img/nav-icon2.svg' alt=""/></a>
+                        <a href="#"><Image width={16} height={16} src='/assets/img/nav-icon3.svg' alt=""/></a>
+                      </div>
+                    </span>
                 </Navbar.Collapse>
             </Container>
             <LoginModal show={showModal} onClose={handleCloseModal}/>
+            <Register onClose={handleCloseRegModal} show={showRegisterModal}/>
         </Navbar>
     );
 };
